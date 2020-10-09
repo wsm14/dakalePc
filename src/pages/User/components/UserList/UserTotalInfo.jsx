d418a5c842413891806de60e1acc7e11b468b813
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2020-10-09 23:20:24 +0800

    用户数据调整

--- src/pages/User/components/UserList/UserTotalInfo.jsx
+++ src/pages/User/components/UserList/UserTotalInfo.jsx
@@ -56,34 +56,34 @@ const UserTotalInfo = ({ dispatch, loading, totalData }) => {
         }}
       ></SearchCondition>
       <Row gutter={16} align="middle">
-        <Col span={12}>
+        {/* <Col span={12}>
           <Spin spinning={!!loading}>
             <Card bordered={false} bodyStyle={styles} style={{ height: 276 }}>
               <Donut data={data} height={276} />
             </Card>
           </Spin>
-        </Col>
-        <Col span={12} style={{ borderLeft: '1px solid #ececec', paddingLeft: 50 }}>
+        </Col> */}
+        <Col span={12}>
           <Card bordered={false} bodyStyle={styles} loading={loading}>
-            <Row gutter={[16, 16]} span={12} align="middle" style={{ height: 276 }}>
+            <Row gutter={[16, 16]} span={12} align="middle" style={{ height: 100, marginLeft: 10 }}>
+              <Col span={8}>
+                <Statistic
+                  title="今日新增用户"
+                  value={totalData.userAddRealNameCount + totalData.userAddTopUpCount}
+                ></Statistic>
+              </Col>
               <Col span={8}>
                 <Statistic title="总注册数" value={totalData.userTotalRegister}></Statistic>
               </Col>
               <Col span={8}>
                 <Statistic title="总实名认证" value={totalData.userRealNameCount}></Statistic>
               </Col>
-              <Col span={8}>
+              {/* <Col span={8}>
                 <Statistic title="总充值用户" value={totalData.userTopUpCount}></Statistic>
-              </Col>
-              <Col span={8}>
-                <Statistic
-                  title="今日新增实名用户"
-                  value={totalData.userAddRealNameCount}
-                ></Statistic>
-              </Col>
-              <Col span={8}>
+              </Col> */}
+              {/* <Col span={8}>
                 <Statistic title="今日新增充值用户" value={totalData.userAddTopUpCount}></Statistic>
-              </Col>
+              </Col> */}
             </Row>
           </Card>
         </Col>
