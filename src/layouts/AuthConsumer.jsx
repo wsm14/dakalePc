12589c7570c2f439b678bc7a564c62d9b622002b
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2020-12-22 20:27:19 +0800

    管理员默认所有权限

--- src/layouts/AuthConsumer.jsx
+++ src/layouts/AuthConsumer.jsx
@@ -3,12 +3,13 @@ import { connect, useLocation } from 'umi';
 const AuthConsumer = (props) => {
   const { menuBtn, children, auth = false, noAuth = null, currentUser } = props;
   const { username = '' } = currentUser;
+  if (auth === true || username == '管理员') return children;
   const authMenu = menuBtn[useLocation().pathname];
   const checkAuth = authMenu ? authMenu.includes(auth) : false;
   // auth 默认没有不显示子组件
   // auth true 默认显示子组件
   // auth 存在时 判断菜单权限内是否包含此权限 包含显示 否则不显示
-  return auth === true || username == '管理员' ? children : checkAuth ? children : noAuth;
+  return checkAuth ? children : noAuth;
 };
 
 export default connect(({ userInfo }) => ({
