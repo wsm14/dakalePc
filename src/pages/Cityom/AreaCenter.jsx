f2fcdd62ce7448249cedda90f09b13f896190783
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-05 10:56:54 +0800

    区县运营中心增加所属省公司搜索

--- src/pages/Cityom/AreaCenter.jsx
+++ src/pages/Cityom/AreaCenter.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { connect } from 'umi';
 import { Button } from 'antd';
+import CITYJSON from '@/common/city';
 import AuthConsumer from '@/layouts/AuthConsumer';
 import HandleSetTable from '@/components/HandleSetTable';
 import DataTableBlock from '@/components/DataTableBlock';
@@ -42,6 +43,12 @@ const AreaCenter = (props) => {
 
   // 搜索参数
   const searchItems = [
+    {
+      label: '所属省公司',
+      name: 'belongProvinceCode',
+      type: 'select',
+      select: { list: CITYJSON.map((item) => ({ name: item.label, value: item.value })) },
+    },
     {
       label: '代理公司名称',
       name: 'partnerName',
