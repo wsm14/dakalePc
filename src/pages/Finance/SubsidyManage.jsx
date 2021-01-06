d1dafd648ba63d64fdc8eb460e18f108ad521cf6
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-06 13:24:11 +0800

    Revert "补贴管理新增"
    This reverts commit 50d7a0a7f66cdde1f3489487c3befd1256bbfc53.

--- src/pages/Finance/SubsidyManage.jsx
+++ src/pages/Finance/SubsidyManage.jsx
@@ -2,18 +2,19 @@ import React, { useRef, useState } from 'react';
 import { connect } from 'umi';
 import { Button } from 'antd';
 import { SHARE_STATUS } from '@/common/constant';
-import exportExcel from '@/utils/exportExcel';
+import Ellipsis from '@/components/Ellipsis';
 import AuthConsumer from '@/layouts/AuthConsumer';
+import PopImgShow from '@/components/PopImgShow';
 import DataTableBlock from '@/components/DataTableBlock';
 import HandleSetTable from '@/components/HandleSetTable';
-import SubsidyDrawer from './components/subsidy/SubsidyDrawer';
 
 const SubsidyManage = (props) => {
   const { shareManage, loading, dispatch } = props;
 
   const childRef = useRef();
   const [visible, setVisible] = useState(false);
-
+  const [visibleHandle, setVisibleHandle] = useState(false);
+  
   // 时间选择器限制选择参数比较
   const [dates, setDates] = useState([]);
   // 时间限制选择一年
@@ -42,6 +43,17 @@ const SubsidyManage = (props) => {
         userMomentIdString: val,
       },
       callback: (detail) => setVisible({ show: true, type, detail }),
+    });
+  };
+
+  // 获取操作日志详情
+  const fetchShareHandleDetail = (val) => {
+    dispatch({
+      type: 'shareManage/fetchShareHandleDetail',
+      payload: {
+        identifyIdStr: val,
+      },
+      callback: (detail) => setVisibleHandle({ show: true, detail }),
     });
   };
 
@@ -77,37 +89,62 @@ const SubsidyManage = (props) => {
   // table 表头
   const getColumns = [
     {
-      title: '序号',
+      title: '种草内容',
       fixed: 'left',
       dataIndex: 'frontImage',
-      render: (val, row, index) => index + 1,
+      render: (val) => <PopImgShow url={val}></PopImgShow>,
     },
     {
-      title: '任务名称',
-      fixed: 'left',
+      title: '内容标题',
       dataIndex: 'title',
       width: 150,
     },
     {
-      title: '补贴类型',
+      title: '所属店铺',
       dataIndex: 'merchantName',
-      render: (val) => val,
+      render: (val) => (
+        <Ellipsis length={10} tooltip>
+          {val}
+        </Ellipsis>
+      ),
     },
     {
-      title: '补贴角色',
+      title: '分享分类',
       align: 'center',
       dataIndex: 'contentType',
       render: (val) => (val == 'video' ? '视频' : '图片'),
     },
     {
-      title: '总参与人数',
+      title: '单次打赏卡豆数',
       align: 'right',
       dataIndex: 'beanAmount',
     },
     {
-      title: '已补贴卡豆数',
+      title: '视频时长',
       align: 'right',
       dataIndex: 'length',
+      render: (val) => (val ? `${val}s` : '--'),
+    },
+    {
+      title: '观看人数',
+      align: 'right',
+      dataIndex: 'viewAmount',
+    },
+    {
+      title: '转发数',
+      align: 'right',
+      dataIndex: 'forwardAmount',
+    },
+    {
+      title: '卡豆支出',
+      align: 'right',
+      dataIndex: 'payedBeanAmount',
+    },
+    {
+      title: '分享状态',
+      align: 'right',
+      dataIndex: 'status',
+      render: (val) => SHARE_STATUS[val],
     },
     {
       title: '操作',
@@ -120,18 +157,19 @@ const SubsidyManage = (props) => {
           <HandleSetTable
             formItems={[
               {
-                type: 'info',
-                click: () => fetchShareDetail(val, record.contentType),
-              },
-              {
-                type: 'del',
+                type: 'down',
                 visible: status == 1 || status == 5,
                 click: () => fetchAuditRefuse(record),
               },
               {
+                type: 'info',
+                click: () => fetchShareDetail(val, record.contentType),
+              },
+              {
                 type: 'own',
-                auth: 'end',
-                title: '结束',
+                auth: 'handleDeatil',
+                title: '操作记录',
+                click: () => fetchShareHandleDetail(val),
               },
             ]}
           />
@@ -154,21 +192,11 @@ const SubsidyManage = (props) => {
     <>
       <DataTableBlock
         btnExtra={({ get }) => (
-          <>
-            <AuthConsumer auth="exportList">
-              <Button className="dkl_green_btn" onClick={() => fetchGetExcel(get())}>
-                导出
-              </Button>
-            </AuthConsumer>
-            <AuthConsumer auth="save">
-              <Button
-                className="dkl_green_btn"
-                onClick={() => setVisible({ type: 'add', show: true })}
-              >
-                新增
-              </Button>
-            </AuthConsumer>
-          </>
+          <AuthConsumer auth="exportList">
+            <Button className="dkl_green_btn" onClick={() => fetchGetExcel(get())}>
+              导出
+            </Button>
+          </AuthConsumer>
         )}
         cRef={childRef}
         loading={loading}
@@ -178,7 +206,6 @@ const SubsidyManage = (props) => {
         dispatchType="shareManage/fetchGetList"
         {...shareManage}
       ></DataTableBlock>
-      <SubsidyDrawer visible={visible} setVisible={setVisible}></SubsidyDrawer>
     </>
   );
 };
