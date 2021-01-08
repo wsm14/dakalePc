5d57ef43d439f0f38f461cdb3ac20d71e7527112
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-08 18:11:34 +0800

    城市新增

--- src/pages/System/components/City/Left/index.jsx
+++ src/pages/System/components/City/Left/index.jsx
@@ -15,7 +15,7 @@ const TradeAreaLeft = ({ cRef, selectCode, setSelectCode }) => {
       selectedKeys={[selectCode.provinceCode]}
       defaultSelectedKeys={[selectCode.provinceCode]}
       onClick={(e) => {
-        const name = e.item.props.children;
+        const name = e.item.props.children[1];
         setSelectCode({
           provinceName: name,
           provinceCode: e.key,
