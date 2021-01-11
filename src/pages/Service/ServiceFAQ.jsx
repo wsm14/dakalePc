c19472d6dc8b2ce8545a1f6406ab0b4994c977c1
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-11 12:30:45 +0800

    faq

---
+++ src/pages/Service/ServiceFAQ.jsx
@@ -0,0 +1,133 @@
+import React, { useRef } from 'react';
+import { connect } from 'umi';
+import { NEWS_STATUS } from '@/common/constant';
+import { Card, Result } from 'antd';
+import AuthConsumer, { authCheck } from '@/layouts/AuthConsumer';
+import Ellipsis from '@/components/Ellipsis';
+import PopImgShow from '@/components/PopImgShow';
+import HandleSetTable from '@/components/HandleSetTable';
+import DataTableBlock from '@/components/DataTableBlock';
+import NewsSet from './components/News/NewsSet';
+
+const ServiceFAQ = (props) => {
+  const { serviceNews, loading, dispatch } = props;
+
+  const childRef = useRef();
+
+  // table 表头
+  const getColumns = [
+    {
+      title: '封面图',
+      fixed: 'left',
+      dataIndex: 'coverImg',
+      render: (val) => <PopImgShow url={val} />,
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      render: (val) => (
+        <Ellipsis length={10} tooltip>
+          {val || '--'}
+        </Ellipsis>
+      ),
+    },
+    {
+      title: '内容简介',
+      dataIndex: 'description',
+      render: (val) => (
+        <Ellipsis length={10} tooltip>
+          {val || '--'}
+        </Ellipsis>
+      ),
+    },
+    {
+      title: '发布人',
+      dataIndex: 'publisherName',
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'createTime',
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: (val) => NEWS_STATUS[val],
+    },
+    {
+      title: '操作',
+      dataIndex: 'newsIdString',
+      fixed: 'right',
+      align: 'right',
+      render: (val, record) => (
+        <HandleSetTable
+          formItems={[
+            {
+              type: 'down',
+              visible: record.status === '1',
+              click: () => fetchNewsStatus({ newsId: val, status: 0 }),
+            },
+            // {
+            //   type: 'own',
+            //   pop: true,
+            //   visible: record.status === '1',
+            //   title: '上架',
+            // },
+          ]}
+        />
+      ),
+    },
+  ];
+
+  // 下架视频
+  const fetchNewsStatus = (payload) => {
+    dispatch({
+      type: 'serviceNews/fetchNewsStatus',
+      payload,
+      callback: () => childRef.current.fetchGetData(),
+    });
+  };
+
+  const tabList = [
+    {
+      key: 'user',
+      auth: 'user',
+      tab: '用户端',
+      content: (
+        <AuthConsumer
+          auth="user"
+          noAuth={<Result status="403" title="403" subTitle="暂无权限"></Result>}
+        >
+          <DataTableBlock
+            CardNone={false}
+            cRef={childRef}
+            loading={loading}
+            columns={getColumns}
+            rowKey={(record) => `${record.newsIdString}`}
+            dispatchType="serviceNews/fetchGetList"
+            {...serviceNews}
+          ></DataTableBlock>
+        </AuthConsumer>
+      ),
+    },
+    {
+      key: 'mre',
+      auth: 'mre',
+      tab: '商家端',
+      content: (
+        <AuthConsumer
+          auth="mre"
+          noAuth={<Result status="403" title="403" subTitle="暂无权限"></Result>}
+        >
+          <NewsSet></NewsSet>
+        </AuthConsumer>
+      ),
+    },
+  ];
+
+  return <Card tabList={authCheck(tabList)}>{authCheck(tabList)[0]['content']}</Card>;
+};
+
+export default connect(({ serviceNews, loading }) => ({
+  serviceNews,
+  loading: loading.models.serviceNews,
+}))(ServiceFAQ);
