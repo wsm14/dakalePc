d6a2d62ab9240bd4849fd8d92aa8c3981874779a
Bingo <592328728@qq.com>
2021-01-17 02:07:07 +0800

    补贴管理-平台直充

--- src/pages/Finance/components/Subsidys/Form/SubsidyDirectMoney.jsx
+++ src/pages/Finance/components/Subsidys/Form/SubsidyDirectMoney.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
 import { SUBSIDY_ROLE } from '@/common/constant';
 import FormCondition from '@/components/FormCondition';
+import MreSelect from './MreSelect';
+import MreSelectShow from './MreSelectShow';
 
 const SubsidyDirectMoney = (props) => {
   const { form, detail } = props;
+  // 选择店铺弹窗
+  const [visible, setVisible] = useState(false);
+  // 选择店铺后回显的数据
+  const [mreList, setMreList] = useState({ keys: [], list: [] });
+
+  // 设置form表单值 商户id
+  useEffect(() => {
+    form.setFieldsValue({ merchantIds: mreList.keys.toString() });
+  }, [mreList]);
 
   const formItems = [
     {
@@ -17,6 +29,28 @@ const SubsidyDirectMoney = (props) => {
       select: Object.keys(SUBSIDY_ROLE).map((item) => ({ name: SUBSIDY_ROLE[item], value: item })),
     },
     {
+      label: '适用商户',
+      name: 'merchantIds',
+      type: 'childrenOwn',
+      childrenOwn: (
+        <Button type="primary" ghost onClick={() => setVisible(true)}>
+          选择店铺
+        </Button>
+      ),
+    },
+    {
+      label: '适用商户',
+      type: 'noForm',
+      childrenOwn: (
+        <MreSelectShow
+          key="MreTable"
+          form={form}
+          {...mreList}
+          setMreList={setMreList}
+        ></MreSelectShow>
+      ),
+    },
+    {
       label: '充值卡豆数',
       name: 'rechargeBeans',
       type: 'number',
@@ -26,7 +60,17 @@ const SubsidyDirectMoney = (props) => {
     },
   ];
 
-  return <FormCondition form={form} formItems={formItems} initialValues={detail}></FormCondition>;
+  return (
+    <>
+      <FormCondition form={form} formItems={formItems} initialValues={detail}></FormCondition>
+      <MreSelect
+        keys={mreList.keys}
+        visible={visible}
+        onOk={(val) => setMreList(val)}
+        onCancel={() => setVisible(false)}
+      ></MreSelect>
+    </>
+  );
 };
 
 export default SubsidyDirectMoney;
--- src/pages/Finance/components/Subsidys/SubsidyDrawer.jsx
+++ src/pages/Finance/components/Subsidys/SubsidyDrawer.jsx
@@ -11,7 +11,7 @@ const SubsidyDrawer = (props) => {
   const { type = 'info', show = false, detail = {} } = visible;
   const [form] = Form.useForm();
 
-  // 确认提交 merchantIds
+  // 确认提交
   const handleUpAudit = () => {
     form.validateFields().then((value) => {
       dispatch({
