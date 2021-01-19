8a08317cd67b5cb9bcf063e87906202c564b0ab6
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-19 10:19:32 +0800

    搜索配置参数容错

--- src/models/Market/SearchSet.js
+++ src/models/Market/SearchSet.js
@@ -24,8 +24,13 @@ export default {
       if (!response) return;
       const { content } = response;
       console.log(content.dictionaryDTO);
-      const listData = JSON.parse(content.dictionaryDTO.extraParam);
-      if (callback) callback(listData ? listData.merchantList.map(item=> ({label: item.merchantName,value: item.id})) : []);
+      const listData = JSON.parse(content.dictionaryDTO.extraParam || '{}');
+      if (callback)
+        callback(
+          listData.merchantList
+            ? listData.merchantList.map((item) => ({ label: item.merchantName, value: item.id }))
+            : [],
+        );
     },
     *fetchSearchSet({ payload, callback }, { call, put }) {
       const response = yield call(fetchSearchSet, payload);
