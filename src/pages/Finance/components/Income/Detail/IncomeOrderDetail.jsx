0551c6a43ab7cbe23a18914a37e4993f8679cef2
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-27 15:43:48 +0800

    收益详情

--- src/pages/Finance/components/Income/Detail/IncomeOrderDetail.jsx
+++ src/pages/Finance/components/Income/Detail/IncomeOrderDetail.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Popover } from 'antd';
 import DrawerCondition from '@/components/DrawerCondition';
 import {
   QrcodeOutlined,
@@ -19,12 +20,44 @@ const IncomeOrderDetail = ({ visible, onClose }) => {
     75: '（无用户/店铺家主）',
   };
 
+  // 公式计算dom
+  const formulaDom = (title = '服务费=店铺实收*服务费比例', footTip) => (
+    <div style={{ lineHeight: 2.5 }}>
+      <div>{title}</div>
+      <span style={{ color: '#00000069' }}>
+        区县分佣=服务费*20%
+        <br />
+        省公司分佣=服务费*5%
+        <br />
+        用户家主分佣=服务费*10%（无家主时分摊到平台）
+        <br />
+        店铺家主分佣=服务费*5%（无家主时分摊到平台）
+        {footTip}
+      </span>
+    </div>
+  );
+
+  // 哒人带货分佣说明
+  const kolformulaDom = (
+    <>
+      <br />
+      哒人分佣=哒人带货利润*90%*哒人等级收益比例
+      <br />
+      哒人家主分佣=哒人带货利润*5%（无家主时分摊到平台）
+      <br />
+      <div style={{ lineHeight: 1.5, whiteSpace: 'pre-line' }}>
+        {`哒人等级收益比例：\nLV0——45%\nLV1——50%\nLV2——55%\nLV3——60%\nLV4——65%\nLV5——70%\nLV6——75%\nLV7——80%\nLV8——90%`}
+      </div>
+    </>
+  );
+
   // 订单详情配置
   const detailProps = {
     scan: {
       type: '扫码支付',
       icon: <QrcodeOutlined />,
       titleKey: 'merchantName',
+      formulaDom,
     },
     goods: {
       type: '商品核销',
@@ -35,26 +68,31 @@ const IncomeOrderDetail = ({ visible, onClose }) => {
       type: '特价商品订单',
       icon: <ShoppingOutlined />,
       titleKey: 'goodsName',
+      formulaDom,
     },
     kolGoods: {
       type: '哒人带货订单',
       icon: <ShoppingOutlined />,
       titleKey: 'goodsName',
+      formulaDom: () => formulaDom('服务费=店铺实收*服务费比例', kolformulaDom),
     },
     reduceCoupon: {
       type: '抵扣券核销',
       icon: <AccountBookOutlined />,
       titleKey: 'couponName',
+      formulaDom,
     },
     marketCoupon: {
       type: '兑换券订单',
       icon: <AccountBookOutlined />,
       titleKey: 'couponName',
+      formulaDom,
     },
     moment: {
       type: '看分享',
       icon: <PlaySquareOutlined />,
       titleKey: 'merchantName',
+      formulaDom: () => formulaDom('推广费=店铺结算卡豆*推广费比例'),
     },
   }[type];
 
@@ -87,7 +125,9 @@ const IncomeOrderDetail = ({ visible, onClose }) => {
           平台佣金比例： {detail.rebate || 0}%{rebate_type[detail.rebate]}
         </div>
         <div className={styles.detail_item}>
-          <a>查看计算公式</a>
+          <Popover content={detailProps.formulaDom()} placement="left">
+            <a>查看计算公式</a>
+          </Popover>
         </div>
       </div>
       <div className={styles.income_order_bottom}>
