bf29b0e87ae4a0b1900ba7a453738d90a3a3532c
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-01-29 10:52:10 +0800

    merchantVerifyId（审核时传）

--- src/pages/Business/components/BusinessList/BusinessEdit.jsx
+++ src/pages/Business/components/BusinessList/BusinessEdit.jsx
@@ -121,7 +121,7 @@ const BusinessAdd = (props) => {
     });
   };
 
-  // 检查店铺信息是否存在 营业执照号 店铺电话提示 userMerchantId merchantName telephone address socialCreditCode
+  // 检查店铺信息是否存在 营业执照号 店铺电话提示 merchantVerifyId（审核时传） userMerchantId merchantName telephone address socialCreditCode
   const fetchMerCheckData = (payload, callback) => {
     dispatch({
       type: 'baseData/fetchMerCheckData',
@@ -178,6 +178,7 @@ const BusinessAdd = (props) => {
     fetchMerCheckData(
       {
         userMerchantId: initialValues.merchantVerifyIdString,
+        merchantVerifyId: initialValues.merchantVerifyIdString,
         telephone,
         socialCreditCode,
       },
