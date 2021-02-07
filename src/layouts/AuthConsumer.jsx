410e2c8ee5cec819b8521d614b1a6d7930a4e684
DESKTOP-CHSEI3M\Administrator <592328728@qq.com>
2021-02-07 17:46:50 +0800

    oss上传文件重构

--- src/layouts/AuthConsumer.jsx
+++ src/layouts/AuthConsumer.jsx
@@ -13,21 +13,24 @@ export const authCheck = (checkAuth = []) => {
 
 /**
  * 权限检查组件
- * @param {*} menuBtn 按钮数据
- * @param {*} auth 权限key 校验 true 则默认显示 flag 全部权限
+ * @param {*} auth 权限key校验 flag 为1全部权限默认显示，auth值为true则默认显示，否则走权限校验存在则显示
  * @param {*} noAuth 无权限时显示内容
- * @param {*} show 是否显示
+ * @param {*} show 是否显示组件
+ * @param {*} menuBtn 按钮数据 model userInfo层获取
  * @param {*} children
  */
 const AuthConsumer = (props) => {
   const { menuBtn, children, auth = false, noAuth = null, flag, show = true } = props;
   if (!show) return '';
+  // 判断是否直接显示 不走权限校验
   if (auth === true || flag === 1) return children;
+  // 获取当前路由下按钮权限
   const authMenu = menuBtn[useLocation().pathname];
+  // 权限校验权限是否存在
   const checkAuth = authMenu ? authMenu.includes(auth) : false;
-  // auth 默认没有不显示子组件
-  // auth true 默认显示子组件
-  // auth 存在时 判断菜单权限内是否包含此权限 包含显示 否则不显示
+  // auth 默认不显示子组件
+  // auth true 显示子组件
+  // 权限不存在时，可显示 noAuth 内容
   return checkAuth ? children : noAuth;
 };
 
