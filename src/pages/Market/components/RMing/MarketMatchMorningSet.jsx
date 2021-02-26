8afeb2bc2c9d2a7c5ffb3ca004afb7e05b4a357c
孙贝贝 <18838986703>
2021-02-26 15:44:05 +0800

    步数和早起组件内DrawerMadalForm替换

--- src/pages/Market/components/RMing/MarketMatchMorningSet.jsx
+++ src/pages/Market/components/RMing/MarketMatchMorningSet.jsx
@@ -1,30 +1,55 @@
+import React from 'react';
+import { connect } from 'umi';
+import { Form, Button } from 'antd';
+import FormCondition from '@/components/FormCondition';
+import DrawerCondition from '@/components/DrawerCondition';
+
 const MarketMatchMorningSet = (props) => {
-  const { dispatch, childRef } = props;
+  const { dispatch, childRef, visible = false, onClose } = props;
+
+  const [form] = Form.useForm();
 
   const fetchMorningSet = (values) => {
-    dispatch({
-      type: 'marketCardRMing/fetchMarketMatchMorningSet',
-      payload: values,
-      callback: () => childRef.current.fetchGetData(),
+    form.validateFields().then((values) => {
+      dispatch({
+        type: 'marketCardRMing/fetchMarketMatchMorningSet',
+        payload: values,
+        callback: () => {
+          onClose();
+          childRef.current.fetchGetData();
+        },
+      });
     });
   };
 
-  return {
-    type: 'Drawer',
-    showType: 'form',
+  const formItems = [
+    {
+      type: 'number',
+      label: '报名卡豆数',
+      name: 'wakeUpSignBeanAmount',
+      extra: '设置成功之后，对下一期生效',
+    },
+  ];
+
+  const modalProps = {
     title: '早起挑战赛设置',
-    loadingModels: 'marketCardRMing',
-    formItems: [
-      {
-        type: 'number',
-        label: '报名卡豆数',
-        name: 'wakeUpSignBeanAmount',
-        extra: '设置成功之后，对下一期生效',
-      },
-    ],
-    onFinish: fetchMorningSet,
-    ...props,
+    visible,
+    onClose,
+    footer: (
+      <Button type="primary" onClick={() => fetchMorningSet()}>
+        确定
+      </Button>
+    ),
   };
+
+  return (
+    <DrawerCondition {...modalProps}>
+      <FormCondition form={form} formItems={formItems}></FormCondition>
+    </DrawerCondition>
+  );
 };
 
-export default MarketMatchMorningSet;
+export default connect(({ marketCardRMing, loading }) => ({
+  marketCardRMing,
+  loading,
+}))(MarketMatchMorningSet);
