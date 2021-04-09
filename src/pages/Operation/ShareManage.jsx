491f07bb0448fc4ed449a3184615cea9afca4b84
D <592328728@qq.com>
2021-04-09 16:55:37 +0800

    时间不修改

--- src/pages/Operation/ShareManage.jsx
+++ src/pages/Operation/ShareManage.jsx
@@ -196,7 +196,7 @@ const ShareManage = (props) => {
     {
       title: '更新时间',
       align: 'center',
-      dataIndex: 'createTime',
+      dataIndex: 'updateTime',
     },
     // {
     //   title: '更新人',
