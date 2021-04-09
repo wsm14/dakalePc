605b191e038c320cc428095dbb0164ca952770d4
D <592328728@qq.com>
2021-04-09 10:55:49 +0800

    隐藏店铺视频统计

--- src/pages/Chart/components/Block/Chart/ActiveChart/index.jsx
+++ src/pages/Chart/components/Block/Chart/ActiveChart/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SaleChart from './SaleChart';
 import BusinessChart from './BusinessChart';
-import BusinessTotal from './BusinessTotal';
+// import BusinessTotal from './BusinessTotal';
 import { Row, Col } from 'antd';
 
 const ActiveChart = () => {
@@ -18,7 +18,7 @@ const ActiveChart = () => {
         {/* 店铺情况（截止昨日）*/}
         <BusinessChart></BusinessChart>
         {/* 店铺视频统计 */}
-        <BusinessTotal></BusinessTotal>
+        {/* <BusinessTotal></BusinessTotal> */}
       </Col>
     </Row>
   );
