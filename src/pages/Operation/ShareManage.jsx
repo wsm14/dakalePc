f39d0f5cb14b0e73183283c01d3340bd4695a936
孙贝贝 <18838986703>
2021-04-14 11:18:42 +0800

    change

--- src/pages/Operation/ShareManage.jsx
+++ src/pages/Operation/ShareManage.jsx
@@ -140,8 +140,8 @@ const ShareManage = (props) => {
               className={styles.sp_span}
               style={{
                 border: '1px solid #CDC9C9',
-                background:'#FAFAFA',
-                color:'#666'
+                background: '#FAFAFA',
+                color: '#666',
               }}
             >
               {BUSINESS_TYPE[val]}
@@ -155,8 +155,8 @@ const ShareManage = (props) => {
               className={styles.sp_span}
               style={{
                 border: '1px solid #FA8072',
-                background:'#FFF0F5',
-                color:'#FA8072'
+                background: '#FFF0F5',
+                color: '#FA8072',
               }}
             >{`${row.topCategoryName}-${row.categoryName}`}</span>
             <span>{`${row.provinceName}-${row.cityName}-${row.districtName}`}</span>
@@ -202,17 +202,17 @@ const ShareManage = (props) => {
     {
       title: '剩余卡豆数',
       align: 'right',
-      dataIndex: 'payedPersonAmount',
+      // dataIndex: 'payedPersonAmount',
       render: (val = 0, row) =>
         Math.round(
           ((row.beanAmount || 0) + (row.exposureBeanAmount || 0)) *
-            ((row.beanPersonAmount || 0) - val),
+            ((row.beanPersonAmount || 0) - row.payedPersonAmount || 0),
         ),
       sorter: (a, b) =>
         ((a.beanAmount || 0) + (a.exposureBeanAmount || 0)) *
-          ((a.beanPersonAmount || 0) - a.payedPersonAmount) -
+          ((a.beanPersonAmount || 0) - (a.payedPersonAmount || 0)) -
         ((b.beanAmount || 0) + (b.exposureBeanAmount || 0)) *
-          ((b.beanPersonAmount || 0) - b.payedPersonAmount),
+          ((b.beanPersonAmount || 0) - (b.payedPersonAmount || 0)),
     },
     {
       title: '关联券/商品',
