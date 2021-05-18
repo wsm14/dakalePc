fc8f4c72aa4aa4deb2228b39b312118b0d9a3011
D <592328728@qq.com>
2021-05-18 17:27:47 +0800

    字段获取

--- src/pages/Operation/components/SpecialGoods/Detail/RegularDetail.jsx
+++ src/pages/Operation/components/SpecialGoods/Detail/RegularDetail.jsx
@@ -65,8 +65,8 @@ const RegularDetail = (props) => {
       render: (val) => COUPON_BUY_RULE[val],
     },
     {
-      label: `单人每天购买份数`,
-      name: 'dayMaxBuyAmount',
+      label: `单人${{ personLimit: '每人', dayLimit: '每天' }[detail.buyRule]}购买份数`,
+      name: { personLimit: 'maxBuyAmount', dayLimit: 'dayMaxBuyAmount' }[detail.buyRule],
       render: (val) => (val && val !== '--' ? `${val}份` : '--'),
     },
     {
