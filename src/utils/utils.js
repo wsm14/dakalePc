d46d8ac72961e92b69662619d3d736e3c0af4d29
D <592328728@qq.com>
2021-05-20 18:41:11 +0800

    城市名称

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 import md5 from 'md5';
 import moment from 'moment';
 import lodash from 'lodash';
+import cityJson from '@/common/cityJson';
 import { parse } from 'querystring';
 import { AUTH_SECRET_KEY } from '@/common/constant';
 
@@ -278,4 +279,25 @@ export const checkFileData = (fileData) => {
       break;
   }
   return aimg;
+};
+
+// 获取城市名
+const getCityName = (code) => {
+  const cityIndex = cityJson.findIndex((item) => item.id === code);
+  return cityJson[cityIndex].name;
+};
+
+// 根据城市code获取城市名称
+export const checkCityName = (code) => {
+  if (!code) return;
+  const codeStr = `${code}`;
+  if (codeStr.length === 2) {
+    return getCityName(codeStr);
+  } else if (codeStr.length === 4) {
+    return `${getCityName(codeStr.slice(0, 2))}-${getCityName(codeStr)}`;
+  } else if (codeStr.length === 6) {
+    return `${getCityName(codeStr.slice(0, 2))}-${getCityName(codeStr.slice(0, 4))}-${getCityName(
+      codeStr,
+    )}`;
+  }
 };
