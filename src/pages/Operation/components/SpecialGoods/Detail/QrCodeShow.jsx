99828e4b0dc75eaa53dec5fa55e824ae3b658e77
D <592328728@qq.com>
2021-05-21 16:38:37 +0800

    商品码

--- src/pages/Operation/components/SpecialGoods/Detail/QrCodeShow.jsx
+++ src/pages/Operation/components/SpecialGoods/Detail/QrCodeShow.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Modal } from 'antd';
 
 const QrCodeShow = ({ url = null, onCancel, title }) => {
-  const changeCanvasToPic = (type, down) => {
+  const changeCanvasToPic = () => {
     let canvas = document.createElement('canvas'); //创建canvas DOM元素
     const ctx = canvas.getContext('2d');
     const img = new Image();
@@ -13,9 +13,10 @@ const QrCodeShow = ({ url = null, onCancel, title }) => {
       canvas.width = img.width;
       ctx.drawImage(img, 0, 0, img.width, img.height);
       const dataURL = canvas.toDataURL('image/png');
-      const downLink = document.getElementById(down);
-      downLink.href = dataURL;
-      downLink.download = `${title}-${type}`; // 图片name
+      const elink = document.createElement('a');
+      elink.href = dataURL;
+      elink.download = `${title}-商品码`; // 图片name
+      elink.click();
       canvas = null;
     };
   };
@@ -24,9 +25,7 @@ const QrCodeShow = ({ url = null, onCancel, title }) => {
     <Modal title={title} visible={url} onCancel={onCancel} footer={null}>
       <img alt="example" style={{ width: '100%' }} src={url} />
       <div style={{ color: '#868686', textAlign: 'right', marginTop: 5 }}>
-        <a id="down_link" onClick={() => changeCanvasToPic('商品码', 'down_link')}>
-          下载
-        </a>
+        <a onClick={() => changeCanvasToPic()}>下载</a>
       </div>
     </Modal>
   );
