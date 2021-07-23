0a592ece17335173b0b26f1fa6bb7939cd8f10a3
D <592328728@qq.com>
2021-07-23 13:51:28 +0800

    lang="zh-cmn-Hans"

--- src/pages/Active/components/template/CreateHtml/index.js
+++ src/pages/Active/components/template/CreateHtml/index.js
@@ -10,7 +10,7 @@ const init = (htmlData = {}) => {
   const { head, body, footer } = createHtml(dataList);
 
   // 网页头部
-  const htmlHeard = `<!DOCTYPE html><html lang="en"><head>
+  const htmlHeard = `<!DOCTYPE html><html lang="zh-cmn-Hans"><head>
   <meta charset="UTF-8"/>
   <meta http-equiv="X-UA-Compatible"content="IE=edge"/>
   <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,minimal-ui, viewport-fit=cover"/>
