a756da391093280e6270801aaa947324ec071a9b
D <592328728@qq.com>
2021-08-07 14:55:13 +0800

    冲突

--- config/env.config.js
+++ config/env.config.js
@@ -4,7 +4,7 @@ const target = 'https://devgateway.dakale.net'
 // const target = 'http://192.168.0.103:6020';
 // const target = 'http://47.114.164.224:6020';
 // const target = 'http://192.168.0.62:6020';
-const target = 'http://192.168.0.86:6020';
+// const target = 'http://192.168.0.86:6020';
 
 export default {
   dev: {
