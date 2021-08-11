0ae745cb79622b8defc5819d0f71673bd90d9c15
孙贝贝 <18838986703>
2021-08-11 15:28:15 +0800

    total

--- src/models/User/CircleMaster.js
+++ src/models/User/CircleMaster.js
@@ -53,7 +53,7 @@ export default {
       yield put({
         type: 'save',
         payload: {
-          masterList: { list: content.voList, total: content.voList.length },
+          masterList: { list: content.voList, total: content.voList?.length },
         },
       });
     },
