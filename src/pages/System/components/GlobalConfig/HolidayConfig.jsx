0c275acf443c30d3684ba977d70b803aaef427d3
孙贝贝 <18838986703>
2021-08-23 16:46:04 +0800

    刷新状态

--- src/pages/System/components/GlobalConfig/HolidayConfig.jsx
+++ src/pages/System/components/GlobalConfig/HolidayConfig.jsx
@@ -82,7 +82,7 @@ const HolidayConfig = (props) => {
     dispatch({
       type: 'globalConfig/fetchFestivalConfigDown',
       payload: { configFestivalId },
-      callback: () => childRef.current.fetchGetData,
+      callback: childRef.current.fetchGetData,
     });
   };
 
