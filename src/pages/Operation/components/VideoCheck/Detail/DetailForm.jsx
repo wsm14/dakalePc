d92bddc0010dac8cfb574d65385ff2d8b6d592dd
孙贝贝 <18838986703>
2021-09-14 17:43:42 +0800

    视频审核

--- src/pages/Operation/components/VideoCheck/Detail/DetailForm.jsx
+++ src/pages/Operation/components/VideoCheck/Detail/DetailForm.jsx
@@ -46,11 +46,11 @@ const DetailForm = (props) => {
     },
     {
       label: '推荐带货',
-      name: 'promotionType',
+      name: 'promotionList',
       render: (val, row) =>
-        ({ reduce: couponsDom(row.contact, '', '', 'valuable'), special: goodsDom(row.contact) }[
-          val
-        ]),
+        val.map((item) =>
+          item.type === 'special' ? goodsDom(item) : couponsDom(item, '', '', item.type),
+        ),
     },
     {
       name: 'gender',
