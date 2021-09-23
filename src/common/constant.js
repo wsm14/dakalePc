df3fd2b4a82dbeebd719c8818f539f7254190bf8
D <592328728@qq.com>
2021-09-23 15:38:39 +0800

    权益订单

--- src/common/constant.js
+++ src/common/constant.js
@@ -325,6 +325,9 @@ export const ORDERS_TYPE = {
   // kolGoods: '哒人带货',
   specialGoods: '周边特惠',
   reduceCoupon: '抵扣券订单',
+  rightGoods: '权益商品',
+  rightCoupon: '权益券',
+  virtualProduct: '虚拟商品',
   // marketCoupon: '兑换券订单',
 };
 
