388891b64a348da345c0e204a45a401cb3ab075e
碧落 <jian964328178@163.com>
2021-11-22 18:23:11 +0800

    完善了upload图片、视频、文件的嵌套格式情况

--- src/components/FormCondition/Upload/Other/index.jsx
+++ src/components/FormCondition/Upload/Other/index.jsx
@@ -21,6 +21,33 @@ const getArrKeyVal = (key, value) => {
   return newVal;
 };
 
+/**
+ * 还原数组对象
+ * @param {*} keyArr 键名
+ * @param {*} val 键值
+ * @param {*} maxFile 最大文件数默认999
+ * @param {*} valueKey 最后一层值键名 不传递则 返回原始值 传递返回 { [valueKey]: val }
+ * @returns
+ */
+const checkArrKeyVal = (keyArr, val, maxFile, valueKey) => {
+  if (!keyArr) return;
+  const newObj = {};
+  let newVal = valueKey ? { [valueKey]: val.slice(0, maxFile || 999) } : val;
+  if (Array.isArray(keyArr)) {
+    for (let index = keyArr.length - 1; index >= 0; index--) {
+      if (index === keyArr.length - 1) {
+        newVal = { [`${keyArr[index]}`]: newVal };
+      } else {
+        newObj[keyArr[index]] = newVal;
+      }
+    }
+    return newObj;
+  } else {
+    newObj[keyArr] = newVal;
+    return newObj;
+  }
+};
+
 // 数据数组还原
 const uploadValues = (fileArr) => {
   return !Array.isArray(fileArr)
@@ -67,11 +94,11 @@ const UploadBlock = (props) => {
         if ((!value.file.status || value.file.status === 'done') && newFileList.length) {
           const newData = newFileList.slice(0, maxFile || 999);
           setFileLists(newData);
-          form.setFieldsValue({ [fileKeyName]: { ...value, fileList: newData } });
+          form.setFieldsValue(checkArrKeyVal(name, newData, maxFile, 'fileList'));
           if (onChange) onChange(value);
         } else {
-          if (!newFileList.length) form.setFieldsValue({ [fileKeyName]: undefined });
-          else form.setFieldsValue({ [fileKeyName]: value });
+          if (!newFileList.length) form.setFieldsValue(checkArrKeyVal(name, undefined));
+          else form.setFieldsValue(checkArrKeyVal(name, value));
           setFileLists(newFileList);
         }
       }}
--- src/components/FormCondition/Upload/Video/index.jsx
+++ src/components/FormCondition/Upload/Video/index.jsx
@@ -40,6 +40,33 @@ const uploadValues = (fileArr) => {
     : fileArr.map((v, i) => imgUrl(v, i));
 };
 
+/**
+ * 还原数组对象
+ * @param {*} keyArr 键名
+ * @param {*} val 键值
+ * @param {*} maxFile 最大文件数默认999
+ * @param {*} valueKey 最后一层值键名 不传递则 返回原始值 传递返回 { [valueKey]: val }
+ * @returns
+ */
+const checkArrKeyVal = (keyArr, val, maxFile, valueKey) => {
+  if (!keyArr) return;
+  const newObj = {};
+  let newVal = valueKey ? { [valueKey]: val.slice(0, maxFile || 999) } : val;
+  if (Array.isArray(keyArr)) {
+    for (let index = keyArr.length - 1; index >= 0; index--) {
+      if (index === keyArr.length - 1) {
+        newVal = { [`${keyArr[index]}`]: newVal };
+      } else {
+        newObj[keyArr[index]] = newVal;
+      }
+    }
+    return newObj;
+  } else {
+    newObj[keyArr] = newVal;
+    return newObj;
+  }
+};
+
 const UploadBlock = (props) => {
   const {
     form,
@@ -123,7 +150,7 @@ const UploadBlock = (props) => {
             if (!isLt1M) {
               message.error(`30s以内不能超过10M， 30秒以上不能超过20M`);
               file.dklFileStatus = 'out';
-              form.setFieldsValue({ [fileKeyName]: undefined });
+              form.setFieldsValue(checkArrKeyVal(name, undefined));
             }
           });
           return false;
@@ -134,11 +161,11 @@ const UploadBlock = (props) => {
           if ((!value.file.status || value.file.status === 'done') && newFileList.length) {
             const newData = newFileList.slice(0, maxFile || 999);
             setFileLists(newData);
-            form.setFieldsValue({ [fileKeyName]: { ...value, fileList: newData } });
+            form.setFieldsValue(checkArrKeyVal(name, newData, maxFile, 'fileList'));
             if (onChange) onChange(value);
           } else {
-            if (!newFileList.length) form.setFieldsValue({ [fileKeyName]: undefined });
-            else form.setFieldsValue({ [fileKeyName]: value });
+            if (!newFileList.length) form.setFieldsValue(checkArrKeyVal(name, undefined));
+            else form.setFieldsValue(checkArrKeyVal(name, value));
             setFileLists(newFileList);
           }
         }}
