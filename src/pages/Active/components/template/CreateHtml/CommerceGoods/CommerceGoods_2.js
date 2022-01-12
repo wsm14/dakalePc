02f602254114d1f465d26642545658afe5ec541e
D <592328728@qq.com>
2022-01-12 16:54:36 +0800

    活动模版升级

--- src/pages/Active/components/template/CreateHtml/CommerceGoods/CommerceGoods_2.js
+++ src/pages/Active/components/template/CreateHtml/CommerceGoods/CommerceGoods_2.js
@@ -77,45 +77,46 @@ ${source
       item.goodsImg
     }) center / cover;"></div>
       <div style="padding:${vw(12)} ${vw(4)} 0">
-      <div style="font-size: ${vw(
-        14,
-      )};color: #333333; white-space:nowrap;text-overflow: ellipsis;overflow: hidden;">${
+        <div style="font-size: ${vw(
+          14,
+        )};color: #333333; white-space:nowrap;text-overflow: ellipsis;overflow: hidden;">${
       item.goodsName
     }</div>
-    <div style="margin-top: ${vw(12)};font-size: ${vw(
+        <div style="margin-top: ${vw(12)};font-size: ${vw(
       12,
-    )}; color: #999999;">原价: <span style="text-decoration: line-through">¥${item.oriPrice}</div>
-
+    )}; color: #999999;">原价: <span style="text-decoration: line-through">¥${item.oriPrice}</span>
+    </div>
     ${
       type === 'self'
-        ? `<div style="lint-height: 1"><div style="margin-top:${vw(6)};font-size: ${vw(
-            10,
-          )};border-radius: ${vw(2)};border: 1px solid #EF476F;color: #EF476F;padding: ${vw(
+        ? `<div style="lint-height: 1">
+          <div style="margin-top:${vw(6)};font-size: ${vw(10)};border-radius: ${vw(
             2,
-          )} ${vw(3)};margin: ${vw(5)} 0 ${vw(
-            4,
-          )};width: max-content;">卡豆价</div><div style="font-size: ${vw(
+          )};border: 1px solid #EF476F;color: #EF476F;padding: ${vw(2)} ${vw(3)};margin: ${vw(
+            5,
+          )} 0 ${vw(4)};width: max-content;">卡豆价</div><div style="font-size: ${vw(
             16,
           )};color: #EF476F;font-weight: bold;">¥${cash}+${bean} <span style="font-size: ${vw(
             13,
-          )}">卡豆</span></div></div>`
+          )}">卡豆</span></div>
+          </div>`
         : `<div style="margin-top:${vw(5)};font-size: ${vw(
             12,
           )};color: #333333;line-height: initial">
         <div>优惠价: <span style="font-size: ${vw(14)}; font-weight: bold">¥${
             item.realPrice
-          }</span></div><div style="height: ${vw(16)};border-radius: ${vw(8)};margin-top: ${vw(
+          }</span></div>
+          <div style="height: ${vw(16)};border-radius: ${vw(8)};margin-top: ${vw(
             4,
           )};display: inline-flex;align-items: center;overflow: hidden;position: relative;">
-          <div style="font-size: ${vw(10)};padding-left: ${vw(4)};padding-right: ${vw(
+              <div style="font-size: ${vw(10)};padding-left: ${vw(4)};padding-right: ${vw(
             3,
           )};height: ${vw(
             16,
           )};background: #ef476f;display: flex;align-items: center;color: #ffffff;;z-index: 1;width: max-content;">卡豆再省</div>
-          <div style="width: 0;height: 0;border-top: ${vw(16)} solid #ef476f;border-right:${vw(
+              <div style="width: 0;height: 0;border-top: ${vw(16)} solid #ef476f;border-right:${vw(
             2,
           )} solid transparent;;z-index: 1"></div>
-          <div style="font-size: ${vw(12)};border-radius: 0 ${vw(50)} ${vw(
+              <div style="font-size: ${vw(12)};border-radius: 0 ${vw(50)} ${vw(
             50,
           )} 0;padding-right: ${vw(
             4,
@@ -126,8 +127,7 @@ ${source
           )};display: flex;align-items: center;max-width:max-content;">￥<div style="white-space: nowrap;text-overflow: ellipsis;overflow: hidden;">${computedPrice(
             item.realPrice,
             payC,
-          )}</div>
-        </div>
+          )}</div></div></div>
         </div>`
     }
       <div style="width: ${vw(154)};height: ${vw(27)};border-radius: ${vw(13)};font-size: ${vw(
@@ -139,7 +139,9 @@ ${source
         type !== 'self' && shareC
           ? `分享赚￥${computedPrice(item.realPrice - item.merchantPrice, shareC)}`
           : '抢购'
-      }</div></div></div>`;
+      }</div>
+      </div>
+      </div>`;
   })
   .join('')}</div>`;
   }
