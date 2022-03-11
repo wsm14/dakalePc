d119adf89e4fdaa985568d6ea10d8f6d193429de
孙贝贝 <18838986703>
2022-03-11 09:50:40 +0800

    antd 升级

--- src/components/FormCondition/InputNumber/index.jsx
+++ src/components/FormCondition/InputNumber/index.jsx
@@ -3,11 +3,16 @@ import { InputNumber } from 'antd';
 import { delectProps } from '../utils';
 
 const InputNumberBlock = (props) => {
-  const { suffix, style, ...ohter } = props;
+  const { suffix, addonAfter, addonBefore, style, ...ohter } = props;
   const divProps = delectProps(ohter);
   return (
     <>
-      <InputNumber style={{ width: suffix ? '75%' : '100%', ...style }} {...divProps} />
+      <InputNumber
+        style={{ width: suffix ? '75%' : '100%', ...style }}
+        addonAfter={addonAfter}
+        addonBefore={addonBefore}
+        {...divProps}
+      />
       {suffix && <span style={{ marginLeft: 10 }}>{suffix}</span>}
     </>
   );
