af8fcd0b6599a9aad34f8ce240c5b46dfbf50f14
D <592328728@qq.com>
2022-05-26 16:55:16 +0800

    基础信息

--- src/pages/SCM/components/SupplierManage/Detail/BaseInfo.jsx
+++ src/pages/SCM/components/SupplierManage/Detail/BaseInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { checkCityName } from '@/utils/utils';
-import { SUPPLIER_AUTH_TYPE } from '@/common/constant';
+import { SUPPLIER_AUTH_TYPE, SUPPLIER_STATUS } from '@/common/constant';
 import DescriptionsCondition from '@/components/DescriptionsCondition';
 
 const BaseInfo = (props) => {
@@ -13,45 +13,47 @@ const BaseInfo = (props) => {
       formItems: [
         {
           label: '供应商类型',
-          name: ['supplierObject', 'type'],
+          name: 'type',
           render: (val) => SUPPLIER_AUTH_TYPE[val],
         },
         {
           label: '供应商名称',
-          name: 'supplierName',
+          name: 'name',
         },
         {
           label: '供应商ID',
-          name: 'identifyId',
+          name: 'id',
         },
         {
           label: '主营类目',
-          name: ['supplierObject', 'classifyNames'],
+          name: 'classifyNames',
         },
         {
           label: '所属地区',
-          name: ['supplierObject', 'districtCode'],
+          name: 'districtCode',
           render: (val) => checkCityName(val),
         },
         {
           label: '详细地址',
-          name: 'createTime',
+          name: 'address',
         },
         {
           label: '入驻时间',
-          name: 'createTime',
+          name: 'settleTime',
         },
         {
           label: '激活时间',
-          name: 'createTime',
+          name: 'activationTime',
         },
         {
           label: '供应商状态',
-          name: 'createTime',
+          name: 'status',
+          render: (val) => SUPPLIER_STATUS[val],
         },
         {
           label: '禁用原因',
-          name: 'createTime',
+          name: 'reason',
+          show: detail.status === '0',
         },
       ],
     },
@@ -60,11 +62,11 @@ const BaseInfo = (props) => {
       formItems: [
         {
           label: '联系人姓名',
-          name: ['supplierObject', 'contactName'],
+          name: 'contactName',
         },
         {
           label: '联系人手机号',
-          name: ['supplierObject', 'contactPhone'],
+          name: 'contactPhone',
         },
       ],
     },
@@ -73,13 +75,13 @@ const BaseInfo = (props) => {
       formItems: [
         {
           label: '有无介绍人',
-          name: ['supplierObject', 'anyInducer'],
+          name: 'anyInducer',
           render: (val) => ['无', '有'][val],
         },
         {
           label: '介绍人账号',
-          name: ['supplierObject', 'inducer'],
-          show: detail?.supplierObject?.anyInducer == 1,
+          name: ['inducer'],
+          show: detail?.anyInducer == 1,
           render: (val) => `${val.inducerName} ${val.inducerMobile} ${val.inducerBeanCode}`,
         },
       ],
