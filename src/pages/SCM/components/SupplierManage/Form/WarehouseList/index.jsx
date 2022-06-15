86ccb36181b6e1016aa65c45001a48dd364c274a
D <592328728@qq.com>
2022-06-15 18:42:00 +0800

    仓库信息

---
+++ src/pages/SCM/components/SupplierManage/Form/WarehouseList/index.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'umi';
+import FormCondition from '@/components/FormCondition';
+
+/**
+ * 仓库信息
+ */
+const WarehouseList = ({ form, dispatch, loading, initialValues }) => {
+  const formItems = [
+    {
+      title: '05 仓库信息',
+      label: '联系人姓名',
+      name: 'contactName',
+    },
+  ];
+
+  return <FormCondition formItems={formItems} form={form} initialValues={initialValues} />;
+};
+
+export default connect(({ loading }) => ({
+  loading,
+}))(WarehouseList);
